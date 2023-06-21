a170e763331cb58009cdba3b36730b271ee293a9
akshaypagar232 <akshaypagar232@gmai.com>
2023-06-21 10:48:37 +0530

    add ProductDto

---
+++ src/main/java/com/bikked/dto/ProductDto.java
@@ -0,0 +1,38 @@
+package com.bikked.dto;
+
+import lombok.*;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+import java.util.Date;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductDto {
+
+    @NotEmpty
+    private String productId;
+    @NotEmpty
+    private String title;
+    @NotEmpty
+    @Size(max = 100,message = "description must be max : 100")
+    private String description;
+    @NotEmpty
+    private long price;
+    @NotEmpty
+    private long discountedPrice;
+    @NotEmpty
+    private long quantity;
+    @NotEmpty
+    private Date addedDate;
+    @NotEmpty
+    private boolean live;
+    @NotEmpty
+    private boolean stock;
+
+}
+
+
+
