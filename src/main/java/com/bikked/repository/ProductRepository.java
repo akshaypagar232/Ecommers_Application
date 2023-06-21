cfd7deabc9882feebd6f006b8f0276cc742350da
akshaypagar232 <akshaypagar232@gmai.com>
2023-06-21 10:46:49 +0530

    add ProductRepository

---
+++ src/main/java/com/bikked/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.bikked.repository;
+
+import com.bikked.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, String> {
+}
