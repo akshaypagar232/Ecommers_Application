50e7496365069fc03b423c7fe451879992cf9a40
akshaypagar232 <akshaypagar232@gmai.com>
2023-06-23 20:25:44 +0530

    add ProductService

--- src/main/java/com/bikked/service/ProductService.java
+++ src/main/java/com/bikked/service/ProductService.java
@@ -2,6 +2,9 @@ package com.bikked.service;
 
 import com.bikked.dto.PageableResponse;
 import com.bikked.dto.ProductDto;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
 
 public interface ProductService {
 
@@ -13,9 +16,11 @@ public interface ProductService {
 
     PageableResponse<ProductDto> getAllProduct(int pageNumber, int pageSize, String sortBy, String sortDirection);
 
-    ProductDto getProductById(ProductDto productDto);
+    ProductDto getProductById(String productId);
 
     PageableResponse<ProductDto> getAllLive(int pageNumber, int pageSize, String sortBy, String sortDirection);
 
+    PageableResponse<ProductDto> searchProduct(String title,int pageNumber, int pageSize, String sortBy, String sortDirection );
 
+    ProductDto createWithCategory(ProductDto productDto,String categoryId);
 }
