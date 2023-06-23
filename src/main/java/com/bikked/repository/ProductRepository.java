50b020f59d2afb05fc92d8c61125f1725a7b2e0f
akshaypagar232 <akshaypagar232@gmai.com>
2023-06-23 20:25:14 +0530

    add ProductRepository

--- src/main/java/com/bikked/repository/ProductRepository.java
+++ src/main/java/com/bikked/repository/ProductRepository.java
@@ -1,9 +1,16 @@
 package com.bikked.repository;
 
 import com.bikked.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, String> {
+
+    Page<Product> findByTitleContaining(String title,Pageable pageable);
+    Page<Product> findByLiveTrue(Pageable pageable);
+
 }
