c238a9af76d76c7dcd4d1c028153e62bef733bf6
akshaypagar232 <akshaypagar232@gmai.com>
2023-06-23 20:17:14 +0530

    add new changes Category

--- src/main/java/com/bikked/entity/Category.java
+++ src/main/java/com/bikked/entity/Category.java
@@ -1,8 +1,11 @@
 package com.bikked.entity;
 
 import lombok.*;
+import org.springframework.beans.factory.annotation.Value;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Setter
@@ -17,13 +20,15 @@ public class Category extends BaseEntity {
 //  @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String categoryId;
 
-    @Column(name = "category_title",nullable = false,length = 50)
+    @Column(name = "category_title", nullable = false, length = 50)
     private String title;
 
-    @Column(name = "category_description",length = 200)
+    @Column(name = "category_description", length = 200)
     private String description;
 
     @Column(name = "cover_image")
     private String coverImage;
 
+    @OneToMany(mappedBy = "category",fetch = FetchType.LAZY,cascade = CascadeType.ALL)      //LAZY used because when fetch category then not fetch product
+    private List<Product> products = new ArrayList<>();
 }
