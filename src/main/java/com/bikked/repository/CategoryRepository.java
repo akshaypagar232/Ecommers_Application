72e7afff87ed9ce34502f201e340b4709b96e679
akshaypagar232 <akshaypagar232@gmai.com>
2023-07-09 20:43:34 +0530

    add new changes CategoryRepository

--- src/main/java/com/bikked/repository/CategoryRepository.java
+++ src/main/java/com/bikked/repository/CategoryRepository.java
@@ -1,17 +1,19 @@
 package com.bikked.repository;
 
-import com.bikked.dto.CategoryDto;
 import com.bikked.entity.Category;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, String> {
 
-    List<Category> findByTitleContaining(String keyword);
+    Page<Category> findByTitleContaining(String keyword, Pageable pageable);
 
-    Category findByTitle(String title);
+    Optional<Category> findByTitle(String title);
 
 }
