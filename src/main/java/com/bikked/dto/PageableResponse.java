0206f2c6ccfdd9c9d2d5ff04bf2cfaf0a43984a6
akshaypagar232 <akshaypagar232@gmai.com>
2023-07-15 16:18:15 +0530

    add new PageableResponse

--- src/main/java/com/bikked/dto/PageableResponse.java
+++ src/main/java/com/bikked/dto/PageableResponse.java
@@ -15,7 +15,7 @@ public class PageableResponse <V>{
     private List<V> content;
     private int pageNumber;
     private int pageSize;
-    private Long totalElements;
+    private long totalElements;
     private int totalPage;
     private boolean lastPage;
 
