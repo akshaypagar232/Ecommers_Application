06abb6f2546729128659c33ddec8f08e1394bc26
akshaypagar232 <akshaypagar232@gmai.com>
2023-07-15 16:14:54 +0530

    add new changes ProductDto

--- src/main/java/com/bikked/dto/ProductDto.java
+++ src/main/java/com/bikked/dto/ProductDto.java
@@ -3,7 +3,9 @@ package com.bikked.dto;
 import com.bikked.entity.Category;
 import lombok.*;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
@@ -21,18 +23,18 @@ public class ProductDto {
     private String title;
     @NotEmpty
     private String description;
-    @NotEmpty
+    @NotNull
     private long price;
-    @NotEmpty
+    @NotNull                               // data type long you should be not used NotEmpty
     private long discountedPrice;
     @NotEmpty
-    private long quantity;
-    @NotEmpty
-    private boolean live;
-    @NotEmpty
-    private boolean stock;
-    @NotEmpty
     private String imageProduct;
+    @NotNull
+    private long quantity;
+    @NotNull
+    private boolean live;
+    @NotNull
+    private boolean stock;
 
     private CategoryDto categoryDto;
 
